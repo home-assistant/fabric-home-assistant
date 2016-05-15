4883edd4174544677b12d0feb9c80f893b4a91e4
Jonathan Baginski <jon@baginski.org>
2016-05-15 12:48:06 -0400

    Installer overhaul. Support for non-virtualenv, single-click operation, MOTD addition

--- fabfile.py
+++ fabfile.py
@@ -8,15 +8,8 @@
 from fabric.api import *
 
 
-env.hosts = [
-    'host.example.com',
-  # 'ip.add.rr.ess
-  # 'server2.domain.tld',
-]
-# Set the username
+env.hosts = ['localhost']
 env.user   = "pi"
-
-# Set the password [NOT RECOMMENDED]
 env.password = "raspberry"
 
 
@@ -60,6 +53,17 @@ def install_syscore():
     sudo("aptitude install -y uuid-dev")
     sudo("aptitude install -y daemon")
     sudo("aptitude install -y curl")
+    sudo("aptitude install -y libgnutls28-dev")
+    sudo("aptitude install -y libgnutlsxx28")
+    sudo("aptitude install -y libgnutls-dev")
+    sudo("aptitude install -y nmap")
+    sudo("aptitude install -y net-tools")
+    sudo("aptitude install -y sudo")
+    sudo("aptitude install -y libglib2.0-dev")
+    sudo("aptitude install -y cython3")
+    sudo("aptitude install -y libudev-dev")
+    sudo("aptitude install -y python3-sphinx")
+    sudo("aptitude install -y python3-setuptools")
 
 def install_pycore():
     """ Download and install VirtualEnv """
@@ -69,6 +73,48 @@ def create_venv():
     """ Create home-assistant VirtualEnv """
     with cd("/srv/hass"):
             sudo("virtualenv -p python3 hass_venv", user="hass")
+
+
+#######################################################
+## Build and Install Applications without VirtualEnv ##
+#######################################################
+
+def setup_homeassistant_novenv():
+    """ Install Home-Assistant """
+    sudo("pip3 install homeassistant", user="hass")
+
+def setup_openzwave_novenv():
+    """ Install python-openzwave """
+    sudo("pip3 install --upgrade cython", user="hass")
+    with cd("/srv/hass/src"):
+        sudo("git clone https://github.com/OpenZWave/python-openzwave.git", user="hass")
+        with cd("python-openzwave"):
+            sudo("git checkout python3", user="hass")
+            sudo("make build", user="hass")
+            sudo("make install", user="hass")
+
+def setup_services_novenv():
+    """ Enable applications to start at boot via systemd """
+    with cd("/etc/systemd/system/"):
+        put("mosquitto.service", "mosquitto.service", use_sudo=True)
+        put("home-assistant_novenv.service", "home-assistant_novenv.service", use_sudo=True)
+
+    sudo("systemctl enable mosquitto.service")
+    sudo("systemctl enable home-assistant_novenv.service")
+    sudo("systemctl daemon-reload")
+
+
+def setup_motd():
+    """ Configures HASS MOTD Banner """
+    with cd("/etc/ssh"):
+        put("sshd_config", "sshd_config", use_sudo=True)
+
+    with cd("/etc/profile.d"):
+        put("motd.sh", "motd.sh", use_sudo=True)
+        sudo("chmod +x motd.sh")
+        sudo("chown root:root motd.sh")
+
+
 
 
 ####################################
@@ -100,16 +146,38 @@ def setup_homeassistant():
     """ Activate Virtualenv, Install Home-Assistant """
     sudo("source /srv/hass/hass_venv/bin/activate && pip3 install homeassistant", user="hass")
 
-def setup_pyzwave():
+def setup_openzwave():
     """ Activate Virtualenv, Install python-openzwave"""
-    sudo("apt-get install -y cython3 libudev-dev python3-sphinx python3-setuptools")
     sudo("source /srv/hass/hass_venv/bin/activate && pip3 install --upgrade cython", user="hass")
     with cd("/srv/hass/src"):
         sudo("git clone https://github.com/OpenZWave/python-openzwave.git", user="hass")
         with cd("python-openzwave"):
             sudo("git checkout python3", user="hass")
-            sudo("source /srv/hass/hass_venv/bin/activate && PYTHON_EXEC=`which python3` make build", user="hass")
-            sudo("source /srv/hass/hass_venv/bin/activate && PYTHON_EXEC=`which python3` make install", user="hass")
+            sudo("source /srv/hass/hass_venv/bin/activate && make build", user="hass")
+            sudo("source /srv/hass/hass_venv/bin/activate && make install", user="hass")
+
+
+def setup_libmicrohttpd():
+    """ Build and install libmicrohttpd """
+    with cd("/srv/hass/src"):
+        sudo("mkdir libmicrohttpd")
+        sudo("chown hass:hass libmicrohttpd")
+        sudo("wget ftp://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.19.tar.gz", user="hass")
+        sudo("tar zxvf libmicrohttpd-0.9.19.tar.gz")
+        with cd("libmicrohttpd-0.9.19"):
+            sudo("./configure")
+            sudo("make")
+            sudo("make install")
+
+def setup_openzwave_controlpanel():
+    """ Build and Install open-zwave-control-panel """
+    with cd("/srv/hass/src"):
+        sudo("git clone https://github.com/OpenZWave/open-zwave-control-panel.git", user="hass")
+        with cd("open-zwave-control-panel"):
+            put("Makefile", "Makefile", use_sudo=True)
+            sudo("make")
+            sudo("ln -sd /srv/hass/hass_venv/lib/python3.4/site-packages/libopenzwave-0.3.0-py3.4-linux-armv7l.egg/config")
+        sudo("chown -R hass:hass /srv/hass/src/open-zwave-control-panel")
 
 def setup_services():
     """ Enable applications to start at boot via systemd """
@@ -119,6 +187,16 @@ def setup_services():
     sudo("systemctl enable mosquitto.service")
     sudo("systemctl enable home-assistant.service")
     sudo("systemctl daemon-reload")
+
+def setup_motd():
+    """ Configures HASS MOTD Banner """
+    with cd("/etc/ssh"):
+        put("sshd_config", "sshd_config", use_sudo=True)
+
+    with cd("/etc/profile.d"):
+        put("motd.sh", "motd.sh", use_sudo=True)
+        sudo("chmod +x motd.sh")
+        sudo("chown root:root motd.sh")
 
 #############
 ## Deploy! ##
@@ -149,7 +227,60 @@ def deploy():
     setup_homeassistant()
 
     ## Activate venv, build and install python-openzwave ##
-    setup_pyzwave()
+    setup_openzwave()
+
+    ## Build and install libmicrohttpd ##
+    setup_libmicrohttpd()
+
+    ## Build and install open-zwave-control-panel ##
+    setup_openzwave_controlpanel()
 
     ## Make apps start at boot ##
     setup_services()
+
+    ## Setup MOTD Banner ##
+    setup_motd
+
+    ## Reboot the system ##
+    reboot()
+
+
+
+
+def deploy_novenv():
+
+    ## Initial Update and Upgrade ##
+    update_upgrade()
+
+    ## Setup service accounts ##
+    setup_users()
+
+    ## Setup directories ##
+    setup_dirs()
+
+    ## Install dependencies ##
+    install_syscore()
+
+    ## Build and Install Mosquitto ##
+    setup_mosquitto()
+
+    ## Activate venv, install Home-Assistant ##
+    setup_homeassistant_novenv()
+
+    ## Activate venv, build and install python-openzwave ##
+    setup_openzwave_novenv()
+
+    ## Build and install libmicrohttpd ##
+    setup_libmicrohttpd()
+
+    ## Build and install open-zwave-control-panel ##
+    setup_openzwave_controlpanel()
+
+    ## Make apps start at boot ##
+    setup_services_novenv()
+
+    ## Setup MOTD Banner ##
+    setup_motd
+
+    ## Reboot the system ##
+    reboot()
---
+++ hass_rpi_installer.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Home Assistant Raspberry Pi Installer Kickstarter
+# Copyright (C) 2016 Jonathan Baginski - All Rights Reserved
+# Permission to copy and modify is granted under the MIT License
+# Last revised 5/15/2016
+
+## Run pre-install apt package dependency checks ##
+PKG_PYDEV=$(dpkg-query -W --showformat='${Status}\n' python-dev|grep "install ok installed")
+echo Checking for python-dev: $PKG_PYDEV
+if [ "" == "$PKG_PYDEV" ]; then
+  echo "No python-dev. Setting up python-dev."
+  sudo apt-get --force-yes --yes install python-dev
+fi
+
+PKG_PYPIP=$(dpkg-query -W --showformat='${Status}\n' python-pip|grep "install ok installed")
+echo Checking for python-pip: $PKG_PYPIP
+if [ "" == "$PKG_PYPIP" ]; then
+  echo "No python-pip. Setting up python-pip."
+  sudo apt-get --force-yes --yes install python-pip
+fi
+
+PKG_GIT=$(dpkg-query -W --showformat='${Status}\n' git|grep "install ok installed")
+echo Checking for python-pip: $PKG_GIT
+if [ "" == "$PKG_GIT" ]; then
+  echo "No git. Setting up git."
+  sudo apt-get --force-yes --yes install git
+fi
+## Ready to go, kick off installer script ##
+python rpi_installer.py
