6b9309134e75892fae1bf8753d1fc02ed896a785
Jonathan Baginski <jon@baginski.org>
2016-05-15 20:26:49 -0400

    virtualev bash flag refactor

--- hass_rpi_installer.sh
+++ hass_rpi_installer.sh
@@ -6,6 +6,49 @@
 
 ## Run pre-install apt package dependency checks ##
 
+while getopts ":n" opt; do
+  case $opt in
+    n)
+
+    me=$(whoami)
+
+    sudo apt-get update
+
+    PKG_PYDEV=$(dpkg-query -W --showformat='${Status}\n' python-dev|grep "install ok installed")
+    echo Checking for python-dev: $PKG_PYDEV
+    if [ "" == "$PKG_PYDEV" ]; then
+      echo "No python-dev. Setting up python-dev."
+      sudo apt-get --force-yes --yes install python-dev
+    fi
+
+    PKG_PYPIP=$(dpkg-query -W --showformat='${Status}\n' python-pip|grep "install ok installed")
+    echo Checking for python-pip: $PKG_PYPIP
+    if [ "" == "$PKG_PYPIP" ]; then
+      echo "No python-pip. Setting up python-pip."
+      sudo apt-get --force-yes --yes install python-pip
+    fi
+
+    PKG_GIT=$(dpkg-query -W --showformat='${Status}\n' git|grep "install ok installed")
+    echo Checking for python-pip: $PKG_GIT
+    if [ "" == "$PKG_GIT" ]; then
+      echo "No git. Setting up git."
+      sudo apt-get --force-yes --yes install git
+    fi
+
+    sudo /usr/bin/pip install fabric
+
+    git clone https://github.com/jbags81/fabric-home-assistant.git
+
+    ( cd /home/$me/fabric-home-assistant && fab deploy_novenv -H localhost )
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+me=$(whoami)
+
 sudo apt-get update
 
 PKG_PYDEV=$(dpkg-query -W --showformat='${Status}\n' python-dev|grep "install ok installed")
@@ -33,6 +76,5 @@ sudo /usr/bin/pip install fabric
 
 git clone https://github.com/jbags81/fabric-home-assistant.git
 
-## Ready to go, kick off installer script ##
-chmod +x /home/pi/fabric-home-assistant/rpi_installer.py
-/usr/bin/python /home/pi/fabric-home-assistant/rpi_installer.py
+
+( cd /home/$me/fabric-home-assistant && fab deploy -H localhost )
